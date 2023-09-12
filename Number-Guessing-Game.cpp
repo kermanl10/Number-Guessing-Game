6dace1947dbf8e8a2c5951af75d4eebbcd036705
kermanl10 <142385154+kermanl10@users.noreply.github.com>
2023-09-12 12:22:51 +0300

    Update Number-Guessing-Game.cpp

--- Number-Guessing-Game.cpp
+++ Number-Guessing-Game.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
     	system("CLS");
     srand(time(nullptr));
-   cout<<"               TAHMIN OYUNUNA HOSGELDINIZ"<<endl;
-    cout<<"Bir Sayi Tuttum 5 Hakkiniz Var Ben Size Yardimci Olucam"<<endl;
+   cout<<"               WELCOME TO THE GUESSING GAME"<<endl;
+    cout<<"I will choose a number, and you will have 5 chances. Your chances will decrease by 1 each time you can't guess, but don't worry, I will help you"<<endl;
     int ary[1],number,YouTah = 0,hak=5;
-     cout<<"          SAYI TUTTUMMM"<<endl;
+     cout<<"          I've chosen a number, let the game begin"<<endl;
     for(int i=0; i<1; i++){
     ary[i]=rand()%49+1;
     number=ary[i];
@@ -18,23 +18,23 @@ int main() {
     cout<<"Tahmininiz:";
     cin>>YouTah;
     if(YouTah==number){
-        cout<<"Tebrikler KAZANDINIZZZZZ TUTTUGUM SAYI "<<number<<" DI"<<endl;
+        cout<<"Congratulations, you won, you guessed the number I had in mind correctly. "<<endl;
         break;
     }
 if(YouTah<number){
-        cout<<"                              Daha Buyuk Bi Sayi Tuttum"<<endl;
+        cout<<"                              I've chosen a number greater than your guess"<<endl;
         hak--;
-         cout<<"                                 Kalan Hakkiniz: "<<hak<<endl;    
+         cout<<"                                 Remaining chances: "<<hak<<endl;    
     }
 if(YouTah>number){
-        cout<<"                              Daha Kucuk Bi Sayi Tuttum"<<endl;
+        cout<<"                              The number I've chosen is smaller than your guess"<<endl;
      hak--;
-     cout<<"                                 Kalan Hakkiniz: "<<hak<<endl;    
+     cout<<"                                 Remaining chances: "<<hak<<endl;    
     }
 
     
     if(hak==0){
-        cout<<"      Malesef Kaybettiniz Tuttugum Sayi: "<<number;
+        cout<<"      Unfortunately, you lost, the number I had in mind: "<<number;
     break;
     }
     }
